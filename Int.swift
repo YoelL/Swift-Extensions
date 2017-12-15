3447c7eb9104529ed96a0c8b28d67d9128e9e968
YoelL <yoellev8@gmail.com>
2017-12-15 15:23:03 +0200

    Update Int.swift

--- Int.swift
+++ Int.swift
@@ -9,6 +9,41 @@ import Foundation
 
 extension Int {
     
+    /// returns number of digits in Int number
+    public var digitCount: Int {
+        get {
+            return numberOfDigits(in: self)
+        }
+    }
+    /// returns number of useful digits in Int number
+    public var usefulDigitCount: Int {
+        get {
+            var count = 0
+            for digitOrder in 0..<self.digitCount {
+                /// get each order digit from self
+                let digit = self % (Int(pow(10, digitOrder + 1) as NSDecimalNumber))
+                    / Int(pow(10, digitOrder) as NSDecimalNumber)
+                if isUseful(digit) { count += 1 }
+            }
+            return count
+        }
+    }
+    // private recursive method for counting digits
+    private func numberOfDigits(in number: Int) -> Int {
+        if abs(number) < 10 {
+            return 1
+        } else {
+            return 1 + numberOfDigits(in: number/10)
+        }
+    }
+    // returns true if digit is useful in respect to self
+    private func isUseful(_ digit: Int) -> Bool {
+        return (digit != 0) && (self % digit == 0)
+    }
+    
+    
+    
+    
     //iterates the closure body a specified number of times
     func times(closure:(Int)-> Void) {
         for i in 0...self {
